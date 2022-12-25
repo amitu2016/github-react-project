2d2839b998123769ef8edf7abc1574cf616dbae1
Amit Upadhyay <amitu2016@gmail.com>
2022-12-25 19:34:42 +0530

    user card ui added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
 import './App.css';
 import React from 'react';
 import Search from './components/Search';
+import UserCard from './components/UserCard';
 
 class App extends React.Component {
 
   state = {
-    user: null
+    user: null,
+    error: null,
+    loading: false
   }
 
   fetchUserData = async username => {
-    try {
+    this.setState({
+      loading: true
+    }, async () => {
+
+       try {
       const res = await fetch(`https://api.github.com/users/${username}`)
 
       if (res.ok) {
         const data = await res.json()
 
         return this.setState({
-          user: data
+          user: data,
+          loading: false
         })
       }
 
-    } catch (error) {
-      console.log(error)
+       const error = (await res.json()).message;
+
+      this.setState({
+        error,
+        loading: false
+      })
+
+    } catch (err) {
+        this.setState({
+        error: "There was some error"
+      })
     }
+
+    })
+   
+
   
   }
 
   render(){
+    const { error , loading, user} = this.state;
     return (
-    <Search fetchData={this.fetchUserData} />
+
+      <div>
+           <Search fetchData={this.fetchUserData} />
+           <div className='text-center pt-5'> 
+             {loading && <p>Loading User Data......</p>}
+             {error && <p className='text-danger'>{error}</p>}
+             {!loading && !error && user && <UserCard user={user}/>}
+           </div>
+      </div>
+
+   
   );
   }
 
