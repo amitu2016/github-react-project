05607bd90d147d9c501ab444f0e58f0cd1159689
Amit Upadhyay <amitu@cdac.in>
2022-12-26 16:16:25 +0530

    removed styling

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,16 +2,11 @@ import React from "react";
 
 const UserCard = ({ user }) => {
 
-    let styles = {
-        marginRight: '20px',
-        width: '200',
-        height: '200',
-      };
 
     return <div className="card mb-4">
     <div className="card-body">
       <div className="d-flex">
-        <div style={styles}>
+        <div>
         <img className="img-round user-image mr-5" src={user.avatar_url}/>
         </div>
         <div>
